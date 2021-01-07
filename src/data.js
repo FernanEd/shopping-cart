10095d1f3a02d3facb1498d981432d71e13a9ce1
FernanEd <fernandoed.bm@gmail.com>
2021-01-07 15:15:24 -0600

    removed alot of images

--- src/data.js
+++ src/data.js
@@ -1,15 +1,5 @@
 function getItems(srcTemplate) {
-  const brands = [
-    'adidas',
-    'lacoste',
-    'polo',
-    'supreme',
-    'boss',
-    'channel',
-    'converse',
-    'nike',
-    'hollister',
-  ];
+  const brands = ['adidas', 'polo', 'nike'];
 
   const colors = [
     'orange',
@@ -27,15 +17,12 @@ function getItems(srcTemplate) {
   let itemArr = [];
 
   let brandIndx = -1;
-  for (let n of [...Array(90).keys()]) {
-    //10 to 99
-    let indx = n + 10;
-    let indxStr = indx.toString();
-    // Get the unit of the current index
+  for (let n of [...Array(30).keys()]) {
+    let indxStr = n.toString();
     let relativeIndx = indxStr.slice(indxStr.length - 1);
 
     // Every 10 indexes, move to next brand
-    if (indx % 10 == 0) brandIndx++;
+    if (n % 10 == 0) brandIndx++;
 
     let item = {
       brand: brands[brandIndx],
@@ -52,6 +39,7 @@ function getItems(srcTemplate) {
 let sweaters = getItems(
   `${process.env.PUBLIC_URL}/images/Sweaters/Sweater_BLANK.png`
 );
+
 let pants = getItems(`${process.env.PUBLIC_URL}/images/Pants/Pants_BLANK.png`);
 let shirts = getItems(
   `${process.env.PUBLIC_URL}/images/Shirts/Shirt_BLANK.png`
